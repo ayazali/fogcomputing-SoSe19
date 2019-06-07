f14107175005a4526fec62adec1e8da99ea16d49
Sujan Tiwari <tiwari.sujan.np@gmail.com>
2019-06-08 00:24:51 +0200

    project structure changed

---
+++ web-node/src/main/java/de/fc/projekt/webnode/WebNodeApplication.java
@@ -0,0 +1,13 @@
+package de.fc.projekt.webnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebNodeApplication.class, args);
+	}
+
+}
