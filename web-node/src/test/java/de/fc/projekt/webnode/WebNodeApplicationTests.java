0a078600155b9ced5cb9062119e9cc168bcdc01a
Sujan Tiwari <tiwari.sujan.np@gmail.com>
2019-06-08 02:49:01 +0200

    project structure changed (#1)
    * project structure changed
    
    * deleted .idea folder

---
+++ web-node/src/test/java/de/fc/projekt/webnode/WebNodeApplicationTests.java
@@ -0,0 +1,16 @@
+package de.fc.projekt.webnode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebNodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
