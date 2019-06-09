a9cbe78bedfc551cc74eeffaad6945c3920a79f9
sujantiwari <tiwari.sujan.np@gmail.com>
2019-06-09 05:18:48 +0200

    Free spot endpoint implementation

--- web-node/src/main/java/de/fc/projekt/webnode/controllers/ParkingController.java
+++ web-node/src/main/java/de/fc/projekt/webnode/controllers/ParkingController.java
@@ -1,6 +1,7 @@
 package de.fc.projekt.webnode.controllers;
 
 import de.fc.projekt.webnode.models.ParkingSpotInfo;
+import de.fc.projekt.webnode.models.ParkingSpotStatus;
 import de.fc.projekt.webnode.repositories.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,10 @@ public class ParkingController {
     public @ResponseBody Iterable<ParkingSpotInfo> GetAllParkingInfos()
     {
         return repository.findAll();
+    }
+    @GetMapping("/spot/free")
+    public @ResponseBody ParkingSpotInfo GetAFreeParkingSpot(){
+        return repository.findByStatus(ParkingSpotStatus.FREE);
     }
     @GetMapping("/spot/{id}")
     public Optional<ParkingSpotInfo> GetParkingSpotInfo(@PathVariable Long id)
