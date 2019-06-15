7c7cc8f96080b8ebd69870c5544259192d092eb0
ayazali <ayazaliqureshi@hotmail.com>
2019-06-15 03:24:16 +0200

    Using application properties for rabbitmq values

--- web-node/src/main/java/de/fc/projekt/webnode/Application.java
+++ web-node/src/main/java/de/fc/projekt/webnode/Application.java
@@ -4,6 +4,7 @@ import de.fc.projekt.webnode.comm.RabbitMQConnector;
 import de.fc.projekt.webnode.models.MQEntry;
 import de.fc.projekt.webnode.repositories.MQEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,6 +15,17 @@ import java.util.concurrent.TimeoutException;
 @SpringBootApplication
 public class Application {
 
+    @Value("${rabbitmq.qname}")
+    private String QUEUE_NAME;
+    @Value("${rabbitmq.hostname}")
+    private String MQ_HOSTNAME;
+    @Value("${rabbitmq.port}")
+    private int MQ_PORT;
+    @Value("${rabbitmq.username}")
+    private String MQ_USERNAME;
+    @Value("${rabbitmq.password}")
+    private String MQ_PASSWORD;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -23,13 +35,14 @@ public class Application {
     MQEntryRepository mqEntryRepository;
     @Autowired
     RabbitMQConnector rabbitMQConnector;
+
     @PostConstruct
     public void addQueueListeners() {
-        MQEntry entry_eins = new MQEntry("192.168.178.47", 5672, "guest", "guest", "ParkenEtage");
+        MQEntry entry_eins = new MQEntry(MQ_HOSTNAME, MQ_PORT, MQ_USERNAME, MQ_PASSWORD, QUEUE_NAME);
         this.mqEntryRepository.save(entry_eins);
 
         rabbitMQConnector.setQUEUE_NAME(entry_eins.getQueueName())
-                .setMQ_HOSTNAME( entry_eins.getHostname())
+                .setMQ_HOSTNAME(entry_eins.getHostname())
                 .setMQ_PORT(entry_eins.getPort())
                 .setMQ_USERNAME(entry_eins.getMqUsername())
                 .setMQ_PASSWORD(entry_eins.getMqPassword());
